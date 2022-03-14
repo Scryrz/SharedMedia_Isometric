0c4313abf6e34ddce8ae51038274e19dde5fd97d
Jacob <45425494+Scryrz@users.noreply.github.com>
2022-03-13 19:05:10 -0700

    Initial commit

---
+++ Soundpaths.lua
@@ -0,0 +1,25 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+-- EXAMPLE: LSM:Register("sound", "|cFFFF00001|r", [[Interface\Addons\SharedMedia_Isometric\sound\FILENAME.ogg]])
+
+LSM:Register("sound", "|cffe800ffAnime Moan|r", [[Interface\Addons\SharedMedia_Isometric\sound\animemoan.ogg]])
+LSM:Register("sound", "|cffe800ffMichael Scott No|r", [[Interface\Addons\SharedMedia_Isometric\sound\michaelscott_nogod.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
