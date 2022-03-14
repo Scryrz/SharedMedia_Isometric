1bd7943ded60158fce680444527532c4c27a55db
Jacob <45425494+Scryrz@users.noreply.github.com>
2022-03-13 23:51:25 -0700

    Add various new sound effects.

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -19,7 +19,10 @@ local LSM = LibStub("LibSharedMedia-3.0")
 
 LSM:Register("sound", "|cff00D4FFAnime Moan|r", [[Interface\Addons\SharedMedia_Isometric\sound\animemoan.ogg]])
 LSM:Register("sound", "|cff00D4FFMichael Scott No|r", [[Interface\Addons\SharedMedia_Isometric\sound\michaelscott_nogod.ogg]])
-
+LSM:Register("sound", "|cffF00D4FFDKP Minus|r", [[Interface\Addons\SharedMedia_Isometric\sound\dkp-minus.ogg]])
+LSM:Register("sound", "|cffF00D4FFKelly Clarkson|r", [[Interface\Addons\SharedMedia_Isometric\sound\kelly-clarkson.ogg]])
+LSM:Register("sound", "|cffF00D4FFSucked More|r", [[Interface\Addons\SharedMedia_Isometric\sound\sucked-more-than-this.ogg]])
+LSM:Register("sound", "|cffF00D4FFMental Problems|r", [[Interface\Addons\SharedMedia_Isometric\sound\you-have-problems.ogg]])
 -- -----
 --   STATUSBAR
 -- -----
