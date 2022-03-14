32e63a86e4110525ee87bd28944502ceb978547e
Jacob <45425494+Scryrz@users.noreply.github.com>
2022-03-13 19:09:15 -0700

    Update colors, initial formatting for Wago publishing.

--- Soundpaths.lua
+++ Soundpaths.lua
@@ -15,10 +15,10 @@ local LSM = LibStub("LibSharedMedia-3.0")
 -- -----
 --   SOUND
 -- -----
--- EXAMPLE: LSM:Register("sound", "|cFFFF00001|r", [[Interface\Addons\SharedMedia_Isometric\sound\FILENAME.ogg]])
+-- EXAMPLE: LSM:Register("sound", "|cffF00D4FF|r", [[Interface\Addons\SharedMedia_Isometric\sound\FILENAME.ogg]])
 
-LSM:Register("sound", "|cffe800ffAnime Moan|r", [[Interface\Addons\SharedMedia_Isometric\sound\animemoan.ogg]])
-LSM:Register("sound", "|cffe800ffMichael Scott No|r", [[Interface\Addons\SharedMedia_Isometric\sound\michaelscott_nogod.ogg]])
+LSM:Register("sound", "|cff00D4FFAnime Moan|r", [[Interface\Addons\SharedMedia_Isometric\sound\animemoan.ogg]])
+LSM:Register("sound", "|cff00D4FFMichael Scott No|r", [[Interface\Addons\SharedMedia_Isometric\sound\michaelscott_nogod.ogg]])
 
 -- -----
 --   STATUSBAR
